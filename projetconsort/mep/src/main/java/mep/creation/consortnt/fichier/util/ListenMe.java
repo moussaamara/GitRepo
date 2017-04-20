c5ef72d68109cd86911d67c7108c68e3e00b5f0d
moussaamara <moussaamara@outlook.fr>
2017-04-20 16:58:17 +0200

    test de de commit

---
+++ projetconsort/mep/src/main/java/mep/creation/consortnt/fichier/util/ListenMe.java
@@ -0,0 +1,13 @@
+package mep.creation.consortnt.fichier.util;
+
+import org.junit.Test;
+
+public class ListenMe {
+	
+	@Test
+	public void Should_show_screen(){
+		System.out.println("Ajout d'un élément de test"+"test d'infos");
+		
+	}
+
+}
